350ad0b6df9c3903e4733db14c5455c2318937f9
Jesnine Erillo <jesnineerillo@berkeley.edu>
2021-05-22 01:09:57 -0700

    Automate Google Calendar events creation from Notion database

--- app.js
+++ app.js
@@ -1,36 +1,92 @@
 require('dotenv').config({ path: './.env' });
-
-const { Client } = require("@notionhq/client");
+const { loadCredentials } = require('./auth');
+const { google, calendar_v3 } = require('googleapis');
+const { Client } = require('@notionhq/client');
 const databaseId = process.env.DATABASE_ID;
 
 // Initializing a client
 const notion = new Client({
-  auth: process.env.NOTION_TOKEN,
+	auth: process.env.NOTION_TOKEN
 });
 
 /**
- * Retrieves each row in database. 
+ * Retrieves each row in database and creates a corresponding event on Google Calendar. 
  */
-const retrieveDB = async () => {
-  try {
-    const eventsToAdd = [];
-    const response = await notion.databases.query({ database_id: databaseId });
-    // Iterate through each element in the database and print properties
-    response.results.forEach(elem => {
-      const vals = {
-        title: elem.properties.Name.title[0].plain_text,
-        date: elem.properties.Dates.date
-      }
-      eventsToAdd.push(vals);
-    });
-    console.log(`Events to be added:`, eventsToAdd)
-    
-    // TODO: Create Google Calendar event from each row in database.
+const createEvents = async (auth, calendar, calendarId) => {
+	try {
+		const eventsToAdd = [];
+		const response = await notion.databases.query({ database_id: databaseId });
+		// Iterate through each element in the database
+		response.results.forEach((elem) => {
+			const prop = elem.properties;
 
-  } catch (error) {
-    console.error(error);
-    process.exit(1);
-  }
-}
+			// Create values for event
+			const title = `[${prop.Class.select.name}] ${prop.Name.title[0].plain_text}`;
+			const start = new Date(prop.Dates.date.start).toISOString().split('T')[0];
+			const end = new Date(prop.Dates.date.end || start).toISOString().split('T')[0];
 
-retrieveDB()
+			// Creates a description of the event using the `Type` and `Link` properties of the Notion element
+			let description = `Type: ${prop.Type.select.name}`;
+			const url = prop.Link ? prop.Link.url : null;
+			if (url) {
+				description.concat(`<br><a href="${url}">${url}</a>`);
+			}
+
+			// Populate eventsToAdd array with relevant values for creating event
+			eventsToAdd.push({
+				summary: title,
+				description: description,
+				start: { date: start },
+				end: { date: end }
+			});
+		});
+
+		console.log(`Events to be added:`, eventsToAdd);
+
+		// Create Google Calendar event from each row in database.
+		eventsToAdd.forEach((event) => {
+			calendar.events.insert(
+				{
+					auth: auth,
+					calendarId: calendarId,
+					resource: event
+				},
+				(error, event) => {
+					if (error) {
+						console.error(error);
+						return;
+					}
+					console.log('Event created: %s', event);
+				}
+			);
+		});
+	} catch (error) {
+		console.error({ error: error.message });
+		process.exit(1);
+	}
+};
+
+/**
+ * Initializes calendar variable for future use after authentication
+ * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ */
+const initCalendar = async (auth) => {
+	try {
+		const calendar = google.calendar({ version: 'v3', auth });
+
+		// Create new calendar to hold events generated from notion
+		const newCalendar = await calendar.calendars.insert({
+			requestBody: {
+				summary: 'Notion Calendar'
+			}
+		});
+		console.log('New Calendar', newCalendar.data);
+
+		createEvents(auth, calendar, newCalendar.data.id);
+	} catch (error) {
+		console.error({ error: error.message });
+	}
+};
+
+// Authenticates the user and creates events in the calendar.
+loadCredentials(initCalendar);
