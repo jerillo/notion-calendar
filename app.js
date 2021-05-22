032ea49c5b40e369111ab1087ee3eccea783d464
Jesnine Erillo <jesnineerillo@berkeley.edu>
2021-05-21 22:42:43 -0700

    Create function to retreieve each element in a database

--- app.js
+++ app.js
@@ -1,8 +1,36 @@
 require('dotenv').config({ path: './.env' });
 
 const { Client } = require("@notionhq/client");
+const databaseId = process.env.DATABASE_ID;
 
 // Initializing a client
 const notion = new Client({
   auth: process.env.NOTION_TOKEN,
 });
+
+/**
+ * Retrieves each row in database. 
+ */
+const retrieveDB = async () => {
+  try {
+    const eventsToAdd = [];
+    const response = await notion.databases.query({ database_id: databaseId });
+    // Iterate through each element in the database and print properties
+    response.results.forEach(elem => {
+      const vals = {
+        title: elem.properties.Name.title[0].plain_text,
+        date: elem.properties.Dates.date
+      }
+      eventsToAdd.push(vals);
+    });
+    console.log(`Events to be added:`, eventsToAdd)
+    
+    // TODO: Create Google Calendar event from each row in database.
+
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+}
+
+retrieveDB()
